8073d5f64d003fdb3eb0e2352c322d00ad69f952
Muhammad Valian Masdani <valianmasdani@gmail.com>
2022-01-30 18:01:43 +0700

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 .venv\diagnostic\Scripts\activate.bat && ^
-py -3 -m pip install -r requirements.txt && ^
+.venv\diagnostic\Scripts\python.exe -m pip install -r requirements.txt && ^
 set FLASK_APP=main.py && ^
-py -3 -m flask run --host=0.0.0.0
+.venv\diagnostic\Scripts\python.exe -m flask run --host=0.0.0.0
