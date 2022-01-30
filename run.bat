4401f1e8a0669a82b79c5e231d610e0e6f6c104c
vmasdani <valianmasdani@gmail.com>
2022-01-30 11:30:16 +0700

    modify python to use local python installation

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 .venv\diagnostic\Scripts\activate.bat && ^
-.venv\diagnostic\Scripts\python.exe -m pip install -r requirements.txt && ^
+python -m pip install -r requirements.txt && ^
 set FLASK_APP=main.py && ^
-.venv\diagnostic\Scripts\python.exe -m flask run --host=0.0.0.0
+python -m flask run --host=0.0.0.0
